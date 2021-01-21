953cba43970735f97a50944c79e7fe8abc740a5d
NamanHegde38 <namanhegde33@gmail.com>
2021-01-22 00:52:01 +0100

    Added Movement System

--- Personal Project - Action-Adventure/Assets/Scripts/PlayerMovement.cs
+++ Personal Project - Action-Adventure/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+     
+     [SerializeField] private float moveSpeed = 5f;
+     [SerializeField] private InputMaster controls;
+     
+     private Vector2 _movement;
+     private Rigidbody2D _rb;
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+     private void Awake() {
+          SetComponents();
+     }
+
+     private void SetComponents() {
+          _rb = GetComponent<Rigidbody2D>();
+     }
+
+     private void Update() {
+          _movement.x = Input.GetAxisRaw("Horizontal");
+          _movement.y = Input.GetAxisRaw("Vertical");
+     }
+
+     private void FixedUpdate() {
+          _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+     }
 }
